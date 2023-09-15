793c5c29477c8ee0422fee26781ce83760e4cb4f
Sajith K <sajithmenon93@gmail.com>
2023-09-15 22:56:14 +0530

    Formatting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { PrismaService } from './services/prisma.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthMiddleware } from './middlewares/auth.middleware';
 import { AuthService } from './services/auth.service';
+import { AIController } from './controllers/ai.controller';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController],
+  controllers: [AppController, AIController],
   providers: [AIService, PrismaService, ConfigService, AuthService],
 })
 export class AppModule {
